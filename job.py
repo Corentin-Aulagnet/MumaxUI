fc97513f79667eabb41a06964b54e523b748d82f
Corentin <corentin.aulagnet@gmail.com>
2023-07-21 13:08:29 -0700

    added a job constructor from a list a job

--- job.py
+++ job.py
@@ -1,11 +1,13 @@
 class Job:
-    def __init__(self,keys,values,title):
-        self.data={}
-        for index,key in keys:
-            self.data[key]=values[index]
-        self.title = title
+    
     def __init__(self,data,title):
         self.data=data
         self.title = title
+    @classmethod
+    def fromLists(cls,keys,values,title):
+        data={}
+        for index,key in keys:
+            data[key]=values[index]
+        cls(data,title)
     def __str__(self):
         return self.title
